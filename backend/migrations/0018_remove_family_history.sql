1f41eecf7a63983b19b9a5ba6ebb50bc63d37d1e
Skufu <agpfrancisco1@gmail.com>
2026-03-05 20:24:15 +0800

    fix: remove broken migrate

--- backend/migrations/0018_remove_family_history.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-ALTER TABLE patients
-    DROP COLUMN IF EXISTS family_history,
-    DROP COLUMN IF EXISTS family_history_diabetes;
-
--- +goose Down
-ALTER TABLE patients
-    ADD COLUMN IF NOT EXISTS family_history BOOLEAN,
-    ADD COLUMN IF NOT EXISTS family_history_diabetes BOOLEAN NOT NULL DEFAULT false;
